b6dcda9b3f8b89cb467463f27dc1e602260c571a
Mateusz Słyszewski <mateuszslyszewski@gmail.com>
2020-03-14 23:59:15 +0100

    start

---
+++ main.js
@@ -0,0 +1,30 @@
+const slideList = [{
+    img: "images/img1.jpg",
+    text: 'Pierwszy tekst'
+},
+{
+    img: "images/img2.jpg",
+    text: 'Drugi tekst'
+},
+{
+    img: "images/img3.jpg",
+    text: 'Trzeci tekst'
+}];
+
+
+const image = document.querySelector('img.slider');
+const h1 = document.querySelector('h1.slider');
+//Interfejs
+const time = 5000;
+let active = 0;
+
+const changeSlide = () => {
+    active++;
+    if (active == slideList.length)
+        image.src = slideList[active].img;
+    h1.textContent = slideList[active].text;
+}
+
+setInterval(changeSlide, time)
+
+// Implementacje
