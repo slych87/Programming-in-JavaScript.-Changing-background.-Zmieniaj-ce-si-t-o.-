3ca05fc8b311185616301455085451b68ad470c1
Mateusz Słyszewski <mateuszslyszewski@gmail.com>
2020-03-15 17:07:53 +0100

    done

--- main.js
+++ main.js
@@ -14,17 +14,28 @@ const slideList = [{
 
 const image = document.querySelector('img.slider');
 const h1 = document.querySelector('h1.slider');
+const dots = [...document.querySelectorAll('.dots span')];
 //Interfejs
 const time = 5000;
 let active = 0;
 
-const changeSlide = () => {
-    active++;
-    if (active == slideList.length)
-        image.src = slideList[active].img;
-    h1.textContent = slideList[active].text;
+const changeDot = () => {
+    const activeDot = dots.findIndex(dot => dot.classList.contains('active'));
+
+    dots[activeDot].classList.remove('active');
+    dots[active].classList.add('active');
 }
 
-setInterval(changeSlide, time)
+const changeSlide = () => {
+    active++;
+    if (active == slideList.length) {
+        active = 0;
+    }
+    image.src = slideList[active].img;
+    h1.textContent = slideList[active].text;
+    changeDot();
+}
+
+setInterval(changeSlide, time);
 
 // Implementacje
